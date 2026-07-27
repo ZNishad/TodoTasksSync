195c79ff7e901c541579f950c53733dc02556604
ZNishad <zulfuqarlinishat@gmail.com>
2026-07-27 12:03:04 +0400

    Initial Commit

---
+++ TodoTasksSync/TodoTasksSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoTasksSyncApp.swift
+//  TodoTasksSync
+//
+//  Created by Nishad Zulfuqarli on 27.07.26.
+//
+
+import SwiftUI
+
+@main
+struct TodoTasksSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoTasksSyncTests/TodoTasksSyncTests.swift
@@ -0,0 +1,19 @@
+//
+//  TodoTasksSyncTests.swift
+//  TodoTasksSyncTests
+//
+//  Created by Nishad Zulfuqarli on 27.07.26.
+//
+
+import Testing
+@testable import TodoTasksSync
+
+struct TodoTasksSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
